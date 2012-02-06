28fa0caeefbc30f24d6b0a9af398e98ea624557e
Alexander Steers <alex.steers@gmail.com>
2012-02-05 20:13:03 -0500

    Insert methods in debug table should give a good enough idea now

--- webapps/cardbook/src/java/com/hextilla/cardbook/auth/DebugTable.java
+++ webapps/cardbook/src/java/com/hextilla/cardbook/auth/DebugTable.java
@@ -98,7 +98,8 @@ public class DebugTable<T> extends Table<T>
         sql.append(")");
         log.info("SQL update prepared statement is: " + sql.toString());
         PreparedStatement insertStmt = conn.prepareStatement(sql.toString());
-        bindUpdateVariables(insertStmt, obj, null, false);
+        log.info("Binding variables for provided object:\n" + obj.toString());
+        bindUpdateVariables(insertStmt, obj, null);
         insertStmt.executeUpdate();
         insertStmt.close();
     }
@@ -122,7 +123,8 @@ public class DebugTable<T> extends Table<T>
         log.info("SQL update prepared statement is: " + sql.toString());
         PreparedStatement insertStmt = conn.prepareStatement(sql.toString());
         for (int i = 0; i < objects.length; i++) {
-            bindUpdateVariables(insertStmt, objects[i], null, false);
+        	log.info("Binding variables for provided object:\n" + objects[i].toString());
+            bindUpdateVariables(insertStmt, objects[i], null);
             insertStmt.addBatch();
         }
         insertStmt.executeBatch();
@@ -161,7 +163,7 @@ public class DebugTable<T> extends Table<T>
     public synchronized int update (Connection conn, T obj, FieldMask mask)
         throws SQLException
     {
-    	return super.delete(conn, obj, mask);
+    	return super.update(conn, obj, mask);
     }
 
     /**
@@ -204,57 +206,6 @@ public class DebugTable<T> extends Table<T>
         return super.delete(conn, objects);
     }
     
-    protected int bindUpdateVariables(PreparedStatement pstmt,
-            T            obj,
-            FieldMask         mask, boolean derp)
-	throws SQLException
-	{
-	return bindUpdateVariables(pstmt, obj, 0, nFields, 0, mask, derp);
-	}
-    
-    protected final int bindUpdateVariables (
-            PreparedStatement pstmt, Object obj, int i, int end, int column,
-            FieldMask mask, boolean derp)
-            throws SQLException
-        {
-            try {
-                while (i < end) {
-                    FieldDescriptor fd = fields[i++];
-                    Object comp = null;
-                    // skip non-modified fields
-                    if (mask != null && !mask.isModified(i-1)) {
-                        continue;
-                    }
-                    if (!fd.isBuiltin() && (comp = fd.field.get(obj)) == null) {
-                        if (fd.isCompound()) {
-                            int nComponents = fd.outType-FieldDescriptor.tCompound;
-                            while (--nComponents >= 0) {
-                                fd = fields[i++];
-                                if (!fd.isCompound()) {
-                                    pstmt.setNull(++column,
-                                                  FieldDescriptor.sqlTypeMapping[fd.outType]);
-                                    log.info("Setting column " + column + " to null");
-                                }
-                            }
-                        } else {
-                            pstmt.setNull(
-                                ++column,
-                                FieldDescriptor.sqlTypeMapping[fd.outType]);
-                            log.info("Setting column " + column + " to null");
-                        }
-                    } else {
-                        if (!fd.bindVariable(pstmt, obj, ++column)) {
-                            int nComponents = fd.outType-FieldDescriptor.tCompound;
-                            column = bindUpdateVariables(
-                                pstmt, comp, i, i+nComponents,column-1, mask);
-                            i += nComponents;
-                        }
-                        log.info("Setting column " + column + " to non-null value " + obj.toString());
-                    }
-                }
-            } catch(IllegalAccessException ex) { throw new IllegalAccessError(); }
-            return column;
-        }
     
     /**
      
