02d69d042302c042ea7f56f000e534cfd0fb0323
Nicholas Vanderzwet <nicholas.vanderzwet@gmail.com>
2012-03-18 21:21:09 -0400

    added fields for rematching to CardBoxObject, may need future changes

--- projects/cardbox/src/main/java/com/hextilla/cardbox/server/CardBoxGameObject.java
+++ projects/cardbox/src/main/java/com/hextilla/cardbox/server/CardBoxGameObject.java
@@ -1,6 +1,8 @@
 package com.hextilla.cardbox.server;
 
 import javax.annotation.Generated;
+
+import com.threerings.parlor.card.data.CardGameObject;
 import com.threerings.parlor.game.data.GameObject;
 
 public class CardBoxGameObject extends GameObject {    
@@ -17,6 +19,10 @@ public class CardBoxGameObject extends GameObject {
     /** The field name of the <code>player2Score</code> field. */
     @Generated(value={"com.threerings.presents.tools.GenDObjectTask"})
     public static final String PLAYER2_SCORE = "player2Score";
+
+    /** The field name of the <code>rematch</code> field. */
+    @Generated(value={"com.threerings.presents.tools.GenDObjectTask"})
+    public static final String REMATCH = "rematch";
     // AUTO-GENERATED: FIELDS END
 
     /** The type of game (AI, Stranger, Friend) **/	
@@ -27,6 +33,19 @@ public class CardBoxGameObject extends GameObject {
 
     // The 2nd players score
     public int player2Score = 0; 
+    
+    /** Indicates that the player has not requested or accepted a rematch. */
+    public static final int NO_REQUEST = 0;
+
+    /** Indicates that the player has requested a rematch. */
+    public static final int REQUESTS_REMATCH = 1;
+
+    /** Indicates that the player has accepted the rematch request. */
+    public static final int ACCEPTS_REMATCH = 2;
+    
+    /** The game state, one of {@link #NO_REQUEST}, {@link #REQUESTS_REMATCH}, or
+     * {@link #ACCEPTS_REMATCH}. */
+    public int rematch = NO_REQUEST;
 
     // AUTO-GENERATED: METHODS START
     /**
@@ -78,6 +97,23 @@ public class CardBoxGameObject extends GameObject {
         requestAttributeChange(
             PLAYER2_SCORE, Integer.valueOf(value), Integer.valueOf(ovalue));
         this.player2Score = value;
+    }
+
+    /**
+     * Requests that the <code>rematch</code> field be set to the
+     * specified value. The local value will be updated immediately and an
+     * event will be propagated through the system to notify all listeners
+     * that the attribute did change. Proxied copies of this object (on
+     * clients) will apply the value change when they received the
+     * attribute changed notification.
+     */
+    @Generated(value={"com.threerings.presents.tools.GenDObjectTask"})
+    public void setRematch (int value)
+    {
+        int ovalue = this.rematch;
+        requestAttributeChange(
+            REMATCH, Integer.valueOf(value), Integer.valueOf(ovalue));
+        this.rematch = value;
     }
     // AUTO-GENERATED: METHODS END
 }
